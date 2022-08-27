19c49c081bf43fc8f66a00926805ed40153ba16a
Padmaja Joshi <padmaja23052002@gmail.com>
2022-08-27 22:41:51 +0530

    Tut-01 successfully completed
    Tut-01 completed

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,8 @@
-def factorial(int x):
-    
+def factorial(x):
+    n=1
+    for i in range (2,x+1):
+        n=n*i
+    print (n)
 
 x=int(input("Enter the number whose factorial is to be found"))
 factorial(x)
