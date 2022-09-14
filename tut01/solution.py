b082c32a47dac35a5139d04bad735ed68faf7e0c
padmaja2305 <padmaja_2001ee94@iitp.ac.in>
2022-09-14 23:11:57 +0530

    solution.py

--- tut01/solution.py
+++ tut01/solution.py
@@ -28,11 +28,11 @@ def octact_identification(mod):
     w_average = df['W'].mean()
 
     # Calculating U', V', W' 
-    df.insert(7, column="U'=U - U avg", value="")
-    df.insert(8, column="V'=V - V avg", value="")
-    df.insert(9, column="W'=W - W avg", value="")
+    df.insert(7, column="U'=U - U average", value="")
+    df.insert(8, column="V'=V - V average", value="")
+    df.insert(9, column="W'=W - W average", value="")
     
     #for i in range(0,rows):
-    df["U'=U - U avg"] = df['U'] - u_avg
-    df["V'=V - V avg"] = df['V'] - v_avg
-    df["W'=W - W avg"] = df['W'] - w_avg
+    df["U'=U - U average"] = df['U'] - u_average
+    df["V'=V - V average"] = df['V'] - v_average
+    df["W'=W - W average"] = df['W'] - w_average
