736b5153134979564693dfdc11ec4d843c50c53b
padmaja2305 <padmaja_2001ee94@iitp.ac.in>
2022-10-16 22:03:07 +0530

    tut4

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -19,3 +19,33 @@ def reading_excel_file(filename):
         print("Error: file not found")
         exit(1)
 
+def average():
+    global data_frame
+    global rows
+    try:
+        # Calculating Average Values
+        u_avg = data_frame['U'].mean()
+        v_avg = data_frame['V'].mean()
+        w_avg = data_frame['W'].mean()
+
+        # Calculating Average Value of U, V, W
+        data_frame.insert(4, column="U Avg", value="")
+        data_frame.insert(5, column="V Avg", value="")
+        data_frame.insert(6, column="W Avg", value="")
+
+        data_frame.at[0, 'U Avg'] = u_avg
+        data_frame.at[0, 'V Avg'] = v_avg
+        data_frame.at[0, 'W Avg'] = w_avg
+
+        # Calculating U', V', W'
+        data_frame.insert(7, column="U'=U - U avg", value="")
+        data_frame.insert(8, column="V'=V - V avg", value="")
+        data_frame.insert(9, column="W'=W - W avg", value="")
+
+        # for i in range(0,rows):
+        data_frame["U'=U - U avg"] = data_frame['U'] - u_avg
+        data_frame["V'=V - V avg"] = data_frame['V'] - v_avg
+        data_frame["W'=W - W avg"] = data_frame['W'] - w_avg
+    except:
+        print("Error in calculating average.")
+        exit()
