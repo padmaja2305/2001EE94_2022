44346c5739a225301bb0bdae5de23fbb0b64ad03
padmaja2305 <padmaja_2001ee94@iitp.ac.in>
2022-11-24 23:14:00 +0530

    tut8

---
+++ tut08/tut8.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from datetime import datetime
+from platform import python_version
+ver = python_version()
+start_time = datetime.now()
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def file_read(file1, file2, file3):
+    try:
+        file_1 = open(file1,'r')
+        Lines_1 = file_1.readlines()
+        file_2 = open(file2, 'r')
+        Lines_2 = file_2.readlines()
+        file_3 = open(file3,'r')
+        Lines_3 = file_3.readlines()
+        runs = {}
+        runs['no run'] = 0
+        runs['1 run'] = 1
+        runs['2 runs'] = 2
+        runs['3 runs'] = 3
+        runs['FOUR'] = 4
+        runs['SIX'] = 6
+    except:
+        print("Error in reading the files")
+        exit()
+    return runs, Lines_1, Lines_2, Lines_3
+
+def Full_nm(nm,lst):
+    for i in range(len(lst)):
+        if nm in lst[i]:
+            break
+    return lst[i]
+
+def nm(nm):
+    nm = nm.split(" ")
+    nm = nm[1:len(nm)+1]
+    nm = " ".join(nm)
+    return nm
+
+def Over(over):
+    over = over.split(" ")
+    over = over[0]
+    return over
+
+def over_num(x):
+    over = ""
+    if x%6==0:
+        over = str(x/6)
+    else:
+        over = str(x//6) + "." +str(x%6)
+    return over
